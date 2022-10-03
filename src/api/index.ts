78866cdc3c1d9b51c50851b9d7cc36a428bd397d
Pedro Leal <pedroxleal10@gmail.com>
2022-10-03 19:43:30 -0300

    feat: add confirm sign up request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,23 @@
 import axios from "axios";
 import { toast } from 'react-hot-toast';
+import ConfirmEmail from "../pages/ConfirmEmail";
 
 const API_URL = `https://api.staging.aca.so`
 
 export async function signUp(data: object) {
   const response = await axios.post(`${API_URL}/auth/sign-up`, data);
-  toast.success(response.data)
+  toast.success('Confirme seu e-mail para prosseguir!')
   return response
 }
 
 export async function sendCodeAgain(email: string) {
   const response = await axios.post(`${API_URL}/auth/resend-confirmation-code`, { email: email})
-  toast.success('O código foi enviado novamente para o email solicitado')
+  toast.success('O código foi enviado novamente para o email solicitado!')
+  return response
+}
+
+export async function confirmSignUp(userEmail: string, code: string) {
+  const response = await axios.post(`${API_URL}/auth/confirm-sign-up`, { email: userEmail, confirmation_code: code })
+  toast.success('Sucesso na confirmação, efetue o login na plataforma!')
   return response
 }
