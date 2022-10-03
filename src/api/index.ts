4c2a4573e8bbda9da6f3e93739b5d581e0c83d7d
Pedro Leal <pedroxleal10@gmail.com>
2022-10-03 19:52:14 -0300

    feat: Requisiçao de login adicionada

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,23 +1,27 @@
 import axios from "axios";
 import { toast } from 'react-hot-toast';
-import ConfirmEmail from "../pages/ConfirmEmail";
 
-const API_URL = `https://api.staging.aca.so`
+const API_URL = `https://api.staging.aca.so`;
 
 export async function signUp(data: object) {
   const response = await axios.post(`${API_URL}/auth/sign-up`, data);
-  toast.success('Confirme seu e-mail para prosseguir!')
-  return response
+  toast.success('Confirme seu e-mail para prosseguir!');
+  return response;
 }
 
 export async function sendCodeAgain(email: string) {
-  const response = await axios.post(`${API_URL}/auth/resend-confirmation-code`, { email: email})
-  toast.success('O código foi enviado novamente para o email solicitado!')
-  return response
+  const response = await axios.post(`${API_URL}/auth/resend-confirmation-code`, { email: email});
+  toast.success('O código foi enviado novamente para o email solicitado!');
+  return response;
 }
 
 export async function confirmSignUp(userEmail: string, code: string) {
-  const response = await axios.post(`${API_URL}/auth/confirm-sign-up`, { email: userEmail, confirmation_code: code })
-  toast.success('Sucesso na confirmação, efetue o login na plataforma!')
-  return response
+  const response = await axios.post(`${API_URL}/auth/confirm-sign-up`, { email: userEmail, confirmation_code: code });
+  toast.success('Sucesso na confirmação, efetue o login na plataforma!');
+  return response;
+}
+
+export async function login(userEmail: string, password: string) {
+  const response = await axios.post(`${API_URL}/auth/login`, { email: userEmail, password: password });
+  return response;
 }
